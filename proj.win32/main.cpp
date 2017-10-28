0317d94daaa228d6e2251efb7d5e7d0e2d28b5ee
chentongjie <3138333534@qq.com>
2017-10-28 22:20:59 +0800

    delete

--- proj.win32/main.cpp
+++ proj.win32/main.cpp
@@ -16,13 +16,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
     AppDelegate app;
     CCEGLView* eglView = CCEGLView::sharedOpenGLView();
     eglView->setViewName("FishingJoy");
-<<<<<<< HEAD
     eglView->setFrameSize(2048, 1536);
 	eglView->setFrameZoomFactor(0.6);
-=======
-    eglView->setFrameSize(2048,1536);
-	//界面的放大系数
-	eglView->setFrameZoomFactor(0.4f);
->>>>>>> 2d8d0953eea3456b4584dfc0425f37dbe1b17e55
     return CCApplication::sharedApplication()->run();
 }
